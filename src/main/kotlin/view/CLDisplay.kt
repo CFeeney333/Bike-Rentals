ab635d66c66ae6cb59b7fc84327c524ffd37b929
ChippySpud <c.feeney333@gmail.com>
2024-10-24 17:59:19 +0100

    Implement showConfirm dialog method
    The showConfirm dialog method presents a title and some content to the console, and prompts for 1 or 2 to be input. The dialog will continue to prompt until 1 or 2 is selected. If invalid input is entered on an iteration, an invalid input message will be shown for the next.

--- src/main/kotlin/view/CLDisplay.kt
+++ src/main/kotlin/view/CLDisplay.kt
@@ -1,8 +1,42 @@
 package view
 
+import utils.clear
+import utils.getInt
+
 class CLDisplay : Display {
+
+    /**
+     * Repeatedly print confirm (yes/no) dialog to the user until valid input
+     *
+     * Return 1 if yes, and 2 if no
+     * Keep prompting user until 1 or 2 selected
+     */
     override fun showConfirm(title: String, content: String): Int {
-        TODO("Not yet implemented")
+        var input: Int? = null
+        var invalid: Boolean = false
+        do {
+            clear()
+            println(title)
+            if (invalid) {
+                println("Invalid option! Please select 1 or 2")
+            }
+            println()
+            println(content)
+            println()
+            println("1) Yes")
+            println("2) No")
+            print(">>> ")
+
+            input = getInt();
+            if (input == 1) {
+                return 1
+            }
+            if (input == 2) {
+                return 2
+            } else {
+                invalid = true
+            }
+        } while (true)
     }
 
     override fun showInput(title: String, content: String, invalid: Boolean): String {
