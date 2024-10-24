cf7afac4337d0231aef1a4c1055da34c24c4cebd
ChippySpud <c.feeney333@gmail.com>
2024-10-24 17:33:56 +0100

    Add documentation to console functions

--- src/main/kotlin/utils/ConsoleAPI.kt
+++ src/main/kotlin/utils/ConsoleAPI.kt
@@ -2,11 +2,21 @@ package utils
 
 const val LINES_TO_SKIP = 40
 
+/**
+ * Return the next line from the console
+ *
+ * Returns null if EOF has been reached before the function has been called
+ */
 fun getLine(prompt: String): String? {
     print(prompt)
     return readlnOrNull()
 }
 
+/**
+ * Return the next integer value from the console
+ *
+ * Returns null if the input is empty or cannot be converted to an Int
+ */
 fun getInt(prompt: String): Int? {
     print(prompt)
     val input: String = readlnOrNull() ?: return null
@@ -19,6 +29,11 @@ fun getInt(prompt: String): Int? {
     }
 }
 
+/**
+ * Return the next double value from the console
+ *
+ * Returns null if the input is empty or cannot be converted to a Double
+ */
 fun getDouble(prompt: String): Double? {
     print(prompt)
     val input: String = readlnOrNull() ?: return null
@@ -31,6 +46,9 @@ fun getDouble(prompt: String): Double? {
     }
 }
 
+/**
+ * Clear the console screen by skipping a certain amount of lines
+ */
 fun clear() {
     for (i in 0 until LINES_TO_SKIP) {
         println()
