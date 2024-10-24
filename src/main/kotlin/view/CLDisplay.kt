815c6e6df3315cad4e407f29e48ec263464b76b6
20107919 <20107919@mail.wit.ie>
2024-10-24 14:10:03 +0100

    Create skeleton for display classes

--- src/main/kotlin/view/CLDisplay.kt
+++ src/main/kotlin/view/CLDisplay.kt
@@ -1,4 +1,20 @@
 package view
 
 class CLDisplay : Display {
+    override fun showConfirm(title: String, content: String): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun showInput(title: String, content: String, invalid: Boolean): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun showMessage(title: String, content: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showOptions(title: String, content: String, options: Array<String>): Int {
+        TODO("Not yet implemented")
+    }
+
 }
--- src/main/kotlin/view/Display.kt
+++ src/main/kotlin/view/Display.kt
@@ -1,4 +1,12 @@
 package view
 
 interface Display {
+
+    fun showConfirm(title: String, content: String): Int
+
+    fun showInput(title: String, content: String, invalid: Boolean): String
+
+    fun showMessage(title: String, content: String): Unit
+
+    fun showOptions(title: String, content: String, options: Array<String>): Int
 }
