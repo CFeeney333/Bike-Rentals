49059d72cd3de32ea2e9ff6db1575f7680b6a8b2
ChippySpud <c.feeney333@gmail.com>
2024-11-07 10:42:17 +0000

    Add Title enum
    A Title represents what goes before the name of a person, signifying their gender

---
+++ src/main/kotlin/model/Title.kt
@@ -0,0 +1,8 @@
+package model
+
+/**
+ * A Title represents what goes before the name of a person, signifying their gender
+ */
+enum class Title(val title: String) {
+    MR("Mr"), MRS("Mrs"), MS("Ms")
+}
