1c1bf463a5b8d1d8b6eebb8d7e9bd1c9df722def
ChippySpud <c.feeney333@gmail.com>
2024-11-07 17:28:18 +0000

    Add kdoc to AccountType

--- src/main/kotlin/model/AccountType.kt
+++ src/main/kotlin/model/AccountType.kt
@@ -1,5 +1,8 @@
 package model
 
+/**
+ * Different account types have different privileges in the system
+ */
 enum class AccountType(type: String) {
     MANAGER("Manager Account"), STAFF("Staff Account")
 }
