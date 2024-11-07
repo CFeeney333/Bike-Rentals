21f90fd3701a0ee378f85786d12c72ce42f3d4de
ChippySpud <c.feeney333@gmail.com>
2024-11-07 10:39:39 +0000

    Create and implement Account class
    An Account represents an account on the system
    
    Each system user has an account, with a username and password

---
+++ src/main/kotlin/model/Account.kt
@@ -0,0 +1,8 @@
+package model
+
+/**
+ * An Account represents an account on the system
+ *
+ * Each system user has an account, with a username and password
+ */
+data class Account(var userName: String, var password: String)
