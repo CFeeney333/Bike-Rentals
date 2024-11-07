26554ee836f9ff35c5e8a22e8e203053407aae01
ChippySpud <c.feeney333@gmail.com>
2024-11-07 10:56:05 +0000

    Create Address data class
    An Address represents a location in the world
    
    An Address has a street name, town name, county name, country name, and zipcode

---
+++ src/main/kotlin/model/Address.kt
@@ -0,0 +1,8 @@
+package model
+
+/**
+ * An Address represents a location in the world
+ *
+ * An Address has a street name, town name, county name, country name, and zipcode
+ */
+data class Address(var street: String, var town: String, var county: String, var country: String, var zipcode: String)
