40d624d1ee97f2fbdf39f0467fbc0f40dd5225be
ChippySpud <c.feeney333@gmail.com>
2024-12-08 12:54:03 +0000

    Create skeletons for tests classes

---
+++ src/test/kotlin/model/BikeModelTest.kt
@@ -0,0 +1,33 @@
+package model
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+ class BikeModelTest {
+
+@BeforeEach
+ fun setUp() {}
+
+@AfterEach
+ fun tearDown() {}
+
+@Test
+ fun updateType() {}
+
+@Test
+ fun updateFrontGears() {}
+
+@Test
+ fun updateBackGears() {}
+
+@Test
+ fun searchByType() {}
+
+@Test
+ fun searchByFrontGears() {}
+
+@Test
+ fun searchByBackGears() {}
+}
